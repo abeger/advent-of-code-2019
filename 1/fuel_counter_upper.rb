ab5a61adbb4dfb3d800bc211aaa286070b67a72e
Andy Beger <abeger@mpr.org>
2019-12-11 09:54:50 -0600

    Day 1

---
+++ 1/fuel_counter_upper.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+def fuel(mass)
+  (mass / 3).floor - 2
+end
+
+def module_fuel(mass)
+  base_fuel = fuel(mass)
+  return 0 if base_fuel <= 0
+
+  base_fuel + module_fuel(base_fuel)
+end
+
+module_masses = File.readlines('input.txt')
+total_fuel = module_masses.sum do |mass|
+  module_fuel(mass.to_f)
+end
+
+
+puts "Total fuel needed:  #{total_fuel}"
