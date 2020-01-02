f34243feb4be50b3688c8d323b063c6e1420a159
Andy Beger <abeger@mpr.org>
2020-01-02 16:20:13 -0600

    First pass at 12.2. Doesn't work

--- 12/n_body.rb
+++ 12/n_body.rb
@@ -9,7 +9,22 @@ end
 
 puts 'Part 1'
 position_text = File.read(ARGV[0])
-sim = NBodyProblem::Simulation.new(position_text)
-sim.run(1000)
+#sim = NBodyProblem::Simulation.new(position_text)
+#sim.run(1000)
 
-puts "Total energy after 1000 steps is #{sim.total_energy}"
+#puts "Total energy after 1000 steps is #{sim.total_energy}"
+
+puts 'Part 2'
+
+csim_x = NBodyProblem::CycleSim.new(position_text, 'x')
+x_steps = csim_x.cycle
+
+csim_y = NBodyProblem::CycleSim.new(position_text, 'y')
+y_steps = csim_y.cycle
+
+csim_z = NBodyProblem::CycleSim.new(position_text, 'z')
+z_steps = csim_z.cycle
+
+total_steps = x_steps.lcm(y_steps).lcm(z_steps)
+
+puts "#{total_steps} until the moons have cycled back"
